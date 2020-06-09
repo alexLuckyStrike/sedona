998e709a52d62861bdd18e96a5427c9f8adfcd4a
Alex <alexbelka1992@mail.ru>
2020-06-09 14:55:28 +0300

    Проект Седона

--- js/catalog.js
+++ js/catalog.js
@@ -1,16 +1,12 @@
-let min = document.querySelector('.min');
-let max = document.querySelector('.max');
-let range = document.querySelector('.filter-form-range').min;
-let value = document.querySelector('.filter-form-range');
+(function() {
+    let min = document.querySelector('.min');
+    let max = document.querySelector('.max');
+    let range = document.querySelector('.filter-form-range').min;
+    let value = document.querySelector('.filter-form-range');
 
+    value.addEventListener('input', function () {
+        max.innerHTML = this.value;
+    });
 
-value.addEventListener('input',function(){
-
-    max.innerHTML = this.value;
-
-});
-
-
-min.innerHTML = range;
-
-
+    min.innerHTML = range;
+}());
--- js/index.js
+++ js/index.js
@@ -1,19 +1,85 @@
 
-let search = document.querySelector('.section-form');
 
-let button = document.querySelector('.button');
+(function() {
 
-let form = document.querySelector('form-search');
+    let search = document.querySelector('.section-form');
+
+    let button = document.querySelector('.button');
+
+    let form = document.querySelector('.form-search');
+
+    let adults = document.getElementById('adults');
+
+    let kids = document.getElementById('kids');
+
+    let adultPlus = document.querySelector('.adult-plus');
+
+    let adultMinus = document.querySelector('.adult-minus');
+
+    let AdultNumber = 0;
+
+    let kidsMinus = document.querySelector('.kids-minus');
+
+    let kidsPlus = document.querySelector('.kids-plus');
+
+    let KidsNumber = 0;
 
 
-button.addEventListener('click',function(e){
-    e.preventDefault();
-    search.classList.toggle('form-search-none');
+    adultPlus.addEventListener('click', function (e) {
+        e.preventDefault();
+        AdultNumber++;
+        adults.value = AdultNumber;
+        if(adults.value < 0){
+            adults.value = 0;
+        }
+    });
 
-});
-let calendar = document.querySelector('.calendar');
-let group = document.querySelectorAll('.field-wrapper');
+    adultMinus.addEventListener('click', function (e) {
+        e.preventDefault();
+        AdultNumber--;
+        adults.value = AdultNumber;
+        if(adults.value < 0){
+            adults.setAttribute('disabled','disabled');
+            adults.value = 0;
+        }
+    });
 
-calendar.addEventListener('click',function(e){
-    e.preventDefault()
-});
+    kidsMinus.addEventListener('click', function (e) {
+        e.preventDefault();
+        KidsNumber--;
+        kids.value = KidsNumber;
+        if(kids.value < 0){
+            kids.setAttribute('disabled','disabled')
+            kids.value = 0
+        }
+    });
+
+    kidsPlus.addEventListener('click', function (e) {
+        e.preventDefault();
+        KidsNumber++;
+        kids.value = KidsNumber;
+        if(kids.value < 0){
+            kids.value = 0
+        }
+    });
+
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+        search.classList.toggle('form-search-none');
+        adults.value = AdultNumber;
+        kids.value = KidsNumber;
+    });
+
+//--не работает удаление класса при нажатии esc
+    window.addEventListener('keydown',function(e){
+       e.preventDefault();
+       if(e.keyCode === 27 && search.classList.contains('form-search-none')){
+           search.classList.remove('form-search-none');
+       }
+    });
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+    });
+
+}());
